761afbe0bc48696399bb96dbd4f1270aebd16218
VanessaADS2111 <118320260+VanessaADS2111@users.noreply.github.com>
2022-11-30 15:26:58 -0300

    Add files via upload
    Implementação e criação das classes Piso e Teo

---
+++ Calculadora.java
@@ -0,0 +1,45 @@
+package poo;
+
+public class Calculadora {
+    private double x;
+
+    private double getX() {
+        return x;
+    }
+
+    private void setX(double x) {
+        this.x = x;
+    }
+
+    public double soma (double x, double y){
+        setX(x + y);
+        return getX();
+    }
+
+    public double subtracao (double x, double y){
+        setX(x - y);
+        return getX();
+    }
+
+    public double multiplicacao(double x, double y){
+            return x * y;
+    }
+    
+    public double divisao(double x, double y){
+        return x / y;
+    }
+    public double potenciacao(double x, double y){
+    	return Math.pow( x, y);
+    }
+    public double raizQuadrada(double x) {
+    	return Math.sqrt(x);
+    }
+
+    public double piso(double x){
+        return Math.floor(x);
+    }
+
+    public double teto(double x){
+         return Math.ceil(x);
+    }
+}
